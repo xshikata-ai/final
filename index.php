431ab56cce872ad7f2ce8f6943d8c2bfd9f7260e
xshikata-ai <xshikata@gmail.com>
2025-11-12 07:34:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,55 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
-// Version
-define('VERSION', '3.0.3.8');
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-// Configuration
-if (is_file('config.php')) {
-    require_once('config.php');
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-// Install
-if (!defined('DIR_APPLICATION')) {
-    header('Location: install/index.php');
-    exit;
-}
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-// Startup
-require_once(DIR_SYSTEM . 'startup.php');
+require __DIR__.'/../vendor/autoload.php';
 
-start('catalog');
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
