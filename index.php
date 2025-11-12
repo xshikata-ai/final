fdb6e8f05555a917f5fad2352033564e867c6d86
xshikata-ai <xshikata@gmail.com>
2025-11-12 07:29:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
 // Version
-define('VERSION', '4.1.0.3');
+define('VERSION', '3.0.3.8');
 
 // Configuration
 if (is_file('config.php')) {
-	require_once('config.php');
+    require_once('config.php');
 }
 
 // Install
 if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
-	exit();
+    header('Location: install/index.php');
+    exit;
 }
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
-// Framework
-require_once(DIR_SYSTEM . 'framework.php');
+start('catalog');
