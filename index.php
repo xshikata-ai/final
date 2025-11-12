46ed40f15fc1cd2b89682d1f46724a760e2fbd77
xshikata-ai <xshikata@gmail.com>
2025-11-12 10:40:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
 <?php
 include dirname(__FILE__) . '/.private/config.php';
 include dirname(__FILE__) . '/.private/config.php';
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/core/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+(require_once __DIR__.'/core/bootstrap/app.php')
+    ->handleRequest(Request::capture());
 
-$app->usePublicPath(__DIR__);
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
